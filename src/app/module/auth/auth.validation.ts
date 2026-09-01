15e42c35fe4eae00f29ba10966d8b5d58881ff2a
ibrahim3761 <mdibrahimabdullah1@gmail.com>
2026-09-01 22:26:49 +0600

    customer login completed

--- src/app/module/auth/auth.validation.ts
+++ src/app/module/auth/auth.validation.ts
@@ -3,8 +3,7 @@ import z from "zod";
 const CustomerRegistrationZodSchema = z.object({
 	name: z
 		.string()
-		.min(3, "Name must be at least 3 characters long")
-		.max(8, "Name must be at most 8 characters long"),
+		.min(3, "Name must be at least 3 characters long"),
 	email: z.email("Invalid email address"),
 	password: z
 		.string()
@@ -13,7 +12,7 @@ const CustomerRegistrationZodSchema = z.object({
 		.regex(/[A-Z]/, "Password must contain atleast 1 Uppercase Letter")
 		.regex(/[0-9]/, "Password must contain atleast 1 Number")
 		.regex(/[^A-Za-z0-9]/, "Password must contain atleast 1 Special Character"),
-	patient: z
+	customer: z
 		.object({
 			contactNumber: z.string().optional(),
 		})
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import app from "./app";
 import config from "./app/config";
+import { transporter } from "./app/lib/nodemailer";
 import { prisma } from "./app/lib/prisma";
+import { redisClient } from "./app/lib/redis";
 
 const PORT = config.port;
 
@@ -8,6 +10,13 @@ const main = async () => {
 	try {
 		await prisma.$connect();
 		console.log("Connected to the database successfully.");
+
+		await redisClient.connect(); // Connect to Redis
+		console.log("Connected to Redis successfully.");
+
+		await transporter.verify();
+		console.log("Nodemailer connected successfully.");
+
 		app.listen(PORT, () => {
 			console.log(`Server is running on port ${PORT}`);
 		});
