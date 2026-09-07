d153ceae0f02f23a0b5cfea7fc725fe33227a607
ibrahim3761 <mdibrahimabdullah1@gmail.com>
2026-09-07 12:47:58 +0600

    fix:replace generic error handling with AppError for validation failures

--- src/app/middleware/validateRequest.ts
+++ src/app/middleware/validateRequest.ts
@@ -1,6 +1,8 @@
 import type { NextFunction, Request, Response } from "express";
 import type z from "zod";
 import { catchAsync } from "../utils/catchAsync";
+import { AppError } from "../utils/AppError";
+import httpStatus from "http-status";
 
 export const validateRequest = (zodSchema: z.ZodObject) => {
 	return catchAsync((req: Request, res: Response, next: NextFunction) => {
@@ -10,10 +12,10 @@ export const validateRequest = (zodSchema: z.ZodObject) => {
 		const result = zodSchema.safeParse(payload);
 
 		if (!result.success) {
-			console.log(result.error);
-			console.log(result.error.issues);
-
-			throw new Error(result.error.issues[0].message);
+			throw new AppError(
+				httpStatus.BAD_REQUEST,
+				result.error.issues[0].message,
+			);
 		}
 
 		req.body = result.data;
